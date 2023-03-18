b44890718a076cd7641a2bfdee1df70bae432171
Rachel Arnao <rachel.e.arnao@gmail.com>
2023-03-18 18:26:28 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 17;
+const num4 = 13;
+
+
+const multiply = num1 * num2;
+
+const mod= num3 % num4;
+
+const max = Math.max(1,2,7,14,20);
+
+const random= Math.floor(Math.random()*10) +1;
+Math.isInterget(random);
+
